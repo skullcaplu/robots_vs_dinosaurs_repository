0f804afaa0261c32338672d0e4e2a38fc7bb982a
Luis Forbes <lu@Luiss-iMac.local>
2022-12-12 19:05:09 -0500

    created a way for the robot and the dinosaur to battle.

--- battlefield.py
+++ battlefield.py
@@ -14,3 +14,27 @@ class Battlefield:
         print(self.robot.health)
         self.robot.health -= self.dinosaur.attack_power
         print(self.robot.health)
+        
+    def attack_dinosaur(self):
+        print(self.dinosaur.health)
+        self.dinosaur.health -= self.robot.weapon.attack_power
+        print(self.dinosaur.health)
+
+    def battle (self):
+        self.robot_attacks = 0
+        self.dinosaur_attacks = 0    
+        while self.dinosaur.health and self.robot.health > 0:
+            self.robot.health -= self.dinosaur.attack_power
+            self.dinosaur_attacks +=1
+            print("dino attacked robot")
+            self.dinosaur.health -= self.robot.weapon.attack_power
+            self.dinosaur.health -= self.robot.weapon.attack_power
+            print("robot attacked dinosaur")
+            self.robot_attacks +=2
+            print(self.robot_attacks)
+            print(self.dinosaur_attacks)
+
+            if self.dinosaur.health == 0:
+                print(f"robot won, remaining health is {self.robot.health}")
+            elif self.robot.health == 0:
+                print(f"dinosaur won, remaining health is {self.dinosaur.health}")
